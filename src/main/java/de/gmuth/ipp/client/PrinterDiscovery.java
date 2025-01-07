419784f7e3f0f247940c6a8d7d482803f5da6a08
gmuth <gerhard.muth@gmx.de>
2025-01-07 10:26:35 +0100

    refactored printer discovery

---
+++ src/main/java/de/gmuth/ipp/client/PrinterDiscovery.java
@@ -0,0 +1,75 @@
+package de.gmuth.ipp.client;
+
+/**
+ * Copyright (c) 2025-2025 Gerhard Muth
+ */
+
+import de.gmuth.log.Logging;
+
+import javax.jmdns.JmDNS;
+import javax.jmdns.ServiceEvent;
+import javax.jmdns.ServiceInfo;
+import javax.jmdns.ServiceListener;
+import java.io.IOException;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.logging.Logger;
+
+import static java.time.Instant.now;
+
+class PrinterDiscovery implements ServiceListener {
+
+    interface Listener {
+        void onPrinterDiscovered(String printerUri);
+    }
+
+    static Logger logger = Logging.getLogger(PrinterDiscovery.class);
+
+    public static void discoverPrinters(Listener aListener) throws InterruptedException, IOException {
+        PrinterDiscovery serviceListener = new PrinterDiscovery(aListener);
+        JmDNS jmDNS = JmDNS.create();
+        jmDNS.addServiceListener("_ipp._tcp.local.", serviceListener);
+        serviceListener.waitUntilIdleFor(Duration.ofSeconds(2));
+        jmDNS.close();
+    }
+
+    private void waitUntilIdleFor(Duration duration) throws InterruptedException {
+        while (Duration.between(lastServiceEvent, now()).get(ChronoUnit.SECONDS) < duration.getSeconds()) {
+            Thread.sleep(100);
+        }
+    }
+
+    private final Listener listener;
+
+    private PrinterDiscovery(Listener aListener) {
+        listener = aListener;
+    }
+
+    private Instant lastServiceEvent = now();
+
+    @Override
+    public void serviceAdded(ServiceEvent serviceEvent) {
+        lastServiceEvent = now();
+    }
+
+    @Override
+    public void serviceRemoved(ServiceEvent serviceEvent) {
+        lastServiceEvent = now();
+    }
+
+    @Override
+    public void serviceResolved(ServiceEvent serviceEvent) {
+        lastServiceEvent = now();
+        ServiceInfo serviceInfo = serviceEvent.getInfo();
+        logger.info("> Discovered printer " + serviceInfo.getName() + ".");
+        String printerUri = String.format("ipp://%s:%d/%s",
+                serviceInfo.getServer(),
+                serviceInfo.getPort(),
+                serviceInfo.getPropertyString("rp")
+        );
+        Logging.flush();
+        listener.onPrinterDiscovered(printerUri);
+    }
+
+}
--- src/main/java/de/gmuth/ipp/client/SavePrinterAttributes.java
+++ src/main/java/de/gmuth/ipp/client/SavePrinterAttributes.java
@@ -1,69 +1,54 @@
 package de.gmuth.ipp.client;
 
 /**
- * Copyright (c) 2024 Gerhard Muth
+ * Copyright (c) 2024-2025 Gerhard Muth
  */
 
 import de.gmuth.ipp.core.IppAttributesGroup;
 import de.gmuth.ipp.core.IppRequest;
 import de.gmuth.ipp.core.IppResponse;
 import de.gmuth.log.Logging;
-import de.gmuth.md.GenerateReadme;
 
-import javax.jmdns.JmDNS;
-import javax.jmdns.ServiceInfo;
 import java.net.URI;
 import java.util.Arrays;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import static de.gmuth.ipp.client.IppMessageRepository.getPrinterMakeAndModel;
 import static de.gmuth.ipp.core.IppOperation.GetPrinterAttributes;
 
 public class SavePrinterAttributes {
 
     static Logger logger = Logging.getLogger(SavePrinterAttributes.class);
-    static private IppMessageRepository ippMessageRepository;
+    static protected IppMessageRepository ippMessageRepository;
+    public boolean forceSaving = false;
 
     public static void main(String[] args) {
         Logging.configure(Level.INFO);
         ippMessageRepository = IppMessageRepository.getInstance();
         try {
-            boolean generateReadme = false;
-            if (args.length > 0) {
-                List<String> argList = Arrays.asList(args);
-                logger.info("> Process printerUri arguments: " + String.join(", ", argList));
-                getAndSavePrinterAttributes(argList);
-            } else {
-                long seconds = 30;
-                logger.info("> Looking for mdns services of type _ipp._tcp.local. (" + seconds + " seconds)");
-                Logging.flush();
-                JmDNS jmDns = JmDNS.create();
-                for (ServiceInfo serviceInfo : jmDns.list("_ipp._tcp.local.", seconds * 1000)) {
-                    getAndSavePrinterAttributes(toUri(serviceInfo));
-                }
-                jmDns.close();
-            }
-            if (generateReadme) new GenerateReadme().generateReadmeFile();
+            new SavePrinterAttributes().getPrinterUrisAndSaveAttributes(args);
+            //new GenerateReadme().generateReadmeFile();
         } catch (Throwable throwable) {
             logger.log(Level.SEVERE, "main failed", throwable);
         }
         Logging.flush();
     }
 
-    private static String toUri(ServiceInfo serviceInfo) {
-        return String.format("ipp://%s:%d/%s",
-                serviceInfo.getServer(),
-                serviceInfo.getPort(),
-                serviceInfo.getPropertyString("rp")
-        );
+    protected void getPrinterUrisAndSaveAttributes(String[] args) throws Exception {
+        if (args.length > 0) {
+            List<String> argList = Arrays.asList(args);
+            logger.info("> printerUris from args: " + String.join(", ", argList));
+            argList.forEach(this::getAndSavePrinterAttributes);
+        } else {
+            logger.info("> Looking for mdns services of type _ipp._tcp.local.");
+            Logging.flush();
+            PrinterDiscovery.discoverPrinters(this::getAndSavePrinterAttributes);
+        }
     }
 
-    static void getAndSavePrinterAttributes(List<String> printerUris) {
-        printerUris.forEach(SavePrinterAttributes::getAndSavePrinterAttributes);
-    }
-
-    static void getAndSavePrinterAttributes(String printerUri) {
+    protected void getAndSavePrinterAttributes(String printerUri) {
         try {
             IppClient ippClient = new IppClient();
             IppRequest ippRequest = ippClient.ippRequest(GetPrinterAttributes, URI.create(printerUri), null, null);
@@ -75,12 +60,18 @@ public class SavePrinterAttributes {
                 logger.info("CUPS printer queues are out of scope. Use the uri of the physical printer instead.");
             } else {
                 logger.fine("Normalize response: " + ippResponse);
-                boolean modified = new AttributesNormalizer(ippResponse).normalize();
-                if (modified) ippMessageRepository.saveIppResponse(ippResponse, true);
+                String makeAndModel = getPrinterMakeAndModel(ippResponse);
+                if (!forceSaving && ippMessageRepository.makeAndModelExists(makeAndModel)) {
+                    logger.info(makeAndModel + " already exists.");
+                } else {
+                    boolean modified = new AttributesNormalizer(ippResponse).normalize();
+                    ippMessageRepository.saveIppResponse(ippResponse, true);
+                }
             }
         } catch (Exception exception) {
             logger.warning("Failed to get attributes from " + printerUri);
             logger.warning(exception.toString());
+            logger.log(Level.FINE, "", exception);
         }
     }
 
@@ -89,5 +80,4 @@ public class SavePrinterAttributes {
             return super.exchange(ippRequest);
         }
     }
-
 }
